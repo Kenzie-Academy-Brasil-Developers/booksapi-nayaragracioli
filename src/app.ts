5584aaab9ba3825a172ac1e57c691b519f3f4532
nayaragracioli <nayaragraciolisa@gmail.com>
2024-01-04 11:38:18 -0300

    feat: Adição Helmet

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express, { json } from "express";
+import helmet from "helmet";
 import { booksRouter } from "./routes/books.routes";
 import { HandleErrors } from "./errors/handleErrors.middleware";
+import "express-async-errors";
 
 export const app = express();
+
+app.use(helmet());
 
 app.use(json());
 
